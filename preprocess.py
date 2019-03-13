c41c29c9c104d0dd2c6c067d820b39f2fe0c530f
MartinXPN <mirakyanmartin@gmail.com>
2019-03-13 18:32:57 +0400

    clean up the code

--- preprocess.py
+++ preprocess.py
@@ -1,44 +1,25 @@
-import getopt
-import inspect
-import os
-import sys
+import fire as fire
+
+SPECIAL_CHAR = '~'
 
 
 class Token:
     def __init__(self, conll_line):
         parts = conll_line.split('\t')
         self.index = int(parts[0])
-        if parts[1] == special_char:
+        if parts[1] == SPECIAL_CHAR:
             parts[1] = '-'
         self.word = parts[1]
-        self.dep_root_index = int(parts[6])
-        self.dep_type = parts[7]
-        self.morph = self.__get_morph(parts)
+        self.lemma = parts[2]
+        self.pos = parts[3]
+        # self.xpos = parts[4]
+        self.morphological_features = parts[5]
+
+        # self.dep_root_index = int(parts[6])
+        # self.dep_type = parts[7]
 
     def __str__(self):
-        return self.word + ': ' + ','.join([self.index, self.morph, self.dep_type])
-
-    def __get_morph(self, parts):
-        pos = parts[3]
-        detailed_pos = parts[4]
-
-        if detailed_pos == "VB-TOINFINITIVE":
-            morph = "tense=TOINFINITIVE"
-        else:
-            morph = parts[5]
-        if morph == '_':
-            return pos
-        if pos == 'BN':  # unify present tense
-            pos = 'VB'
-            morph += "|tense=BEINONI"
-        morph_parts = morph.split('|')
-        if len(morph_parts) == 5:
-            morph_parts = ['gen=MF'] + morph_parts[2:]  # fix 'MF' gender format
-        morph = '-'.join([pos] + [p.split('=')[1] for p in morph_parts])
-        if 'POS' in detailed_pos:  # unify possessives
-            morph += '-B'
-
-        return morph
+        return self.word + ': ' + ','.join([self.index, self.word, self.lemma, self.pos, self.morphological_features])
 
 
 class Sentence:
@@ -50,50 +31,14 @@ class Sentence:
             print(token)
 
 
-special_char = '~'
+def preprocess(input_path, output_path):
 
+    def token_format(t):
+        return SPECIAL_CHAR.join(['w:' + t.word, 'm:' + t.morphological_features])
 
-class ModelCreator:
-    def __init__(self, should_filter, token_format):
-        self.should_filter = should_filter
-        self.token_format = token_format
-
-
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
-    except getopt.GetoptError:
-        print('test.py -i <inputfile> -o <outputfile>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -i <inputfile> -o <outputfile>')
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-
-    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    parentdir = os.path.dirname(currentdir)
-    sys.path.insert(0, parentdir + '/utils')
-    import utils
-    word2count = utils.get_word2count(inputfile)
-
-    def get_count(word):
-        if word not in word2count:
-            return 0
-        return word2count[word]
-
-    def is_marker(word):
-        return word in ['*PRP*', '*POS*', '*DEF*', '*ACC*']
-
-    should_filter = lambda word: get_count(word) < 5 or is_marker(word)
-    token_format = lambda t: special_char.join(['w:' + t.word, 'm:' + t.morph])
-
-    with open(inputfile, 'rb') as rf, open(outputfile, 'w') as wf:
+    with open(input_path, 'r') as rf, open(output_path, 'w') as wf:
         line = rf.readline()
-        while line != '':
+        while line.strip() != '':
             conll_lines = []
             while line.strip() != '':
                 conll_lines.append(line)
@@ -102,10 +47,10 @@ def main(argv):
                 line = rf.readline()
                 continue
             sentence = Sentence(conll_lines)
-            tokens = [t for t in sentence.tokens if not should_filter(t.word)]
+            tokens = [t for t in sentence.tokens]
             wf.write(' '.join([token_format(t) for t in tokens]) + '\n')
             line = rf.readline()
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    fire.Fire(preprocess)
--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 input_file_url="https://raw.githubusercontent.com/UniversalDependencies/UD_Armenian-ArmTDP/master/hy_armtdp-ud-train.conllu"
-input_file=$script_dir/${input_file_url##*/}
+input_file=${script_dir}/${input_file_url##*/}
 
-if [ ! -f $input_file ]
+if [ ! -f ${input_file} ]
 then
-  wget -c $input_file_url
+  wget -c ${input_file_url}
 fi
 
 
@@ -16,20 +16,20 @@ corpus_conll=${input_file}_filtered
 grep -Ev '^#' ${input_file} > ${corpus_conll}
 corpus_processed=${corpus_conll}_processed
 
-python preprocess.py -i $corpus_conll -o $corpus_processed
+python preprocess.py --input_path ${corpus_conll} --output_path ${corpus_processed}
 
 
 # The contents of
 # ./train_evaluate.sh $corpus_processed
 props="w+m"
-model_path=${corpus_processed}_$props
-model_full_path=$model_path.vec
+model_path=${corpus_processed}_${props}
+model_full_path=${model_path}.vec
 
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-training_dir=$script_dir/fasttext
+training_dir=${script_dir}/fasttext
 exe_name=fasttext
 
-(cd $training_dir; [ -e $exe_name ] && rm $exe_name; make; ./$exe_name skipgram -input $corpus_processed -output $model_path -props $props -lr 0.025 -dim 200 -ws 2 -epoch 5 -minCount 5 -neg 5 -loss ns -bucket 2000000 -thread 1 -t 1e-3 -lrUpdateRate 100)
+(cd ${training_dir}; [ -e ${exe_name} ] && rm ${exe_name}; make; ./${exe_name} skipgram -input ${corpus_processed} -output ${model_path} -props ${props} -lr 0.025 -dim 200 -ws 2 -epoch 5 -minCount 5 -neg 5 -loss ns -bucket 2000000 -thread 1 -t 1e-3 -lrUpdateRate 100)
 
 # Do not evaluate the armenian model as there is no data yet
 # ./evaluate.sh $model_full_path
