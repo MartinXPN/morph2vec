147f5ec04965fa3718a9a7847ecc37e344489271
MartinXPN <mirakyanmartin@gmail.com>
2019-03-14 17:13:34 +0400

    added word2morph to preprocessing step which can be ommited if paths to the model and processor.pkl are not provided

--- preprocess.py
+++ preprocess.py
@@ -1,36 +1,41 @@
-import json
 import re
-from typing import Iterable, Dict, Tuple
+from typing import Iterable, List, Tuple, Optional
 
 import fire as fire
 from nltk import everygrams
+from word2morph.entities.sample import Sample
+from word2morph.predict import Word2Morph
 
 SPECIAL_CHAR = '~'
 
 
-def parse(conll_lines: Iterable[str], word2morphemes: Dict[str, Tuple[str]], min_ngram_len: int, max_ngram_len: int):
+def parse(conll_lines: Iterable[str],
+          min_ngram_len: int, max_ngram_len: int,
+          word2morphemes: Optional[Word2Morph] = None):
 
     class Token:
         def __init__(self, conll_line: str):
             parts = conll_line.split('\t')
-            self.index = int(parts[0])
-            self.word = parts[1].replace(SPECIAL_CHAR, '-')
-            self.lemma = parts[2].replace(SPECIAL_CHAR, '-')
-            self.pos = parts[3]
-            # self.xpos = parts[4]
-            self.morphological_tags = parts[5].split('|')
-            self.morphemes = word2morphemes[self.lemma] if self.lemma in word2morphemes else tuple()
-            self.ngrams = [''.join(g) for g in everygrams(self.word, min_len=min_ngram_len, max_len=max_ngram_len)]
-            # self.dep_root_index = int(parts[6])
+            self.index: int = int(parts[0])
+            self.word: str = parts[1].replace(SPECIAL_CHAR, '-')
+            self.lemma: str = parts[2].replace(SPECIAL_CHAR, '-')
+            self.pos: str = parts[3]
+            # self.xpos: str = parts[4]
+            self.morphological_tags: List[str] = parts[5].split('|')
+            self.morphemes: Tuple[str] = tuple()
+            self.ngrams: List[str] = [''.join(g) for g in everygrams(self.word,
+                                                                     min_len=min_ngram_len,
+                                                                     max_len=max_ngram_len)]
+            # self.dep_root_index: int = int(parts[6])
             # self.dep_type = parts[7]
 
     def token_format(t: Token):
         res = SPECIAL_CHAR.join([
             'w:' + t.word,
             'l:' + t.lemma,
-            SPECIAL_CHAR.join(['t:' + mt for mt in t.morphological_tags + ['POS=' + t.pos]]),
-            SPECIAL_CHAR.join(['m:' + m for m in t.morphemes]),
-            SPECIAL_CHAR.join(['n:' + g for g in t.ngrams]),
+            SPECIAL_CHAR.join(['t:' + str(mt) for mt in t.morphological_tags + ['POS=' + t.pos]]),
+            SPECIAL_CHAR.join(['m:' + str(m) for m in t.morphemes]),
+            SPECIAL_CHAR.join(['n:' + str(g) for g in t.ngrams]),
         ])
         # Replace consecutive duplicate SPECIAL_CHARs
         res = re.sub(r"{}+".format(SPECIAL_CHAR), SPECIAL_CHAR, res)
@@ -39,13 +44,22 @@ def parse(conll_lines: Iterable[str], word2morphemes: Dict[str, Tuple[str]], min
         return res
 
     tokens = [Token(line) for line in conll_lines if line.split('\t')[0].isdigit()]
+
+    ''' Add morpheme information (lemma2morph) '''
+    if word2morphemes:
+        morphemes = word2morphemes.predict(inputs=[Sample(word=tok.lemma, segments=tuple()) for tok in tokens],
+                                           batch_size=1)
+        for m, tok in zip(morphemes, tokens):
+            tok.morphemes = m.segments
     return ' '.join([token_format(t) for t in tokens]) + '\n'
 
 
-def preprocess(input_path: str, output_path: str, word2morphemes_path: str = None,
+def preprocess(input_path: str, output_path: str,
+               word2morphemes_model_path: str = None, word2morphemes_processor_path: str = None,
                min_ngram_len: int = 3, max_ngram_len: int = 6):
 
-    word2morphemes = json.load(word2morphemes_path) if word2morphemes_path else dict()
+    word2morphemes = {} if word2morphemes_model_path is None or word2morphemes_processor_path is None \
+        else Word2Morph(model_path=word2morphemes_model_path, processor_path=word2morphemes_processor_path)
 
     with open(input_path, 'r', encoding='utf-8') as rf, open(output_path, 'w', encoding='utf-8') as wf:
         line = rf.readline()
